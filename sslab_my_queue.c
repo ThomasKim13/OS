3b7fc3febcc352c0767e4efaae56420d1d51f44f
ThomasKim13 <98933250+ThomasKim13@users.noreply.github.com>
2024-04-10 02:37:57 -0700

    Update sslab_my_queue.c

--- sslab_my_queue.c
+++ sslab_my_queue.c
@@ -6,28 +6,32 @@
 static int queue[QUEUE_SIZE];
 static int front = -1;
 static int rear = -1;
-
+//Simple enqueue function that is learned from Data Structures course
 static int enqueue(int value) {
     if ((rear + 1) % QUEUE_SIZE == front) {
         printk(KERN_INFO "Enqueue failed: Queue full\n");
         return -1;
+	    //the enqueue algorithim
     } else {
         if (front == -1) {
             front = 0;
         }
         rear = (rear + 1) % QUEUE_SIZE;
         queue[rear] = value;
+	    //Added print function for dmesg to show that the Syscall works
         printk(KERN_INFO "Enqueued value: %d\n", value);
 	printk(KERN_INFO "QUEUE SYSCALL \n");
         return 0;
     }
 }
-
+//Simple dequeue function that is learned from Data Structures Course
 static int dequeue(void) {
     int value;
+	//if there is nothing in the queue then the error is handled to avoid SegFaults
     if (front == -1) {
         printk(KERN_INFO "Dequeue failed: Queue empty\n");
         return -1;
+	    //The dequeue algorithim
     } else {
         value = queue[front];
         if (front == rear) {
@@ -35,6 +39,7 @@ static int dequeue(void) {
         } else {
             front = (front + 1) % QUEUE_SIZE;
         }
+	    //Added print function for dmesg to show that the Syscall works
         printk(KERN_INFO "Dequeued value: %d\n", value);
 	printk(KERN_INFO "DEQUEUE SYSCALL \n");
         return value;
